e7cbfac66e7fe5cac7a459cf2ce9283eae1e6c20
Sultan Moldobekov <gazy_95@mail.ru>
2023-05-08 10:42:29 +0600

    Создана новая миграцияю. Создана  новая миграция, потому что старая миграция была создана некорректно.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,90 +3,91 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using home_56.ViewModels;
 
 namespace home_56.Controllers
 {
     public class AccountController : Controller
     {
-        private StoreContext db;
-        public AccountController(StoreContext _db)
-        {
-            db = _db;
-        }
-        [HttpGet]
-        public IActionResult Login()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                User user = await db.Users.Include(u => u.Role).FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
-                if (user != null)
-                {
-                    await Authenticate(user);
-                    return RedirectToAction("Index", "Home");
-                }
-            }
-            return View(model);
-        }
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
-                if (user == null)
-                {
-                    var role = await db.Roles.FirstOrDefaultAsync(r => r.Name == "user");
-                    var newuser = new User
-                    {
-                        Email = model.Email,
-                        Password = model.Password,
-                        Role = role,
-                        RoleId = role.Id
-                    };
-                    await db.Users.AddAsync(newuser);
-                    await db.SaveChangesAsync();
-                    await Authenticate(newuser);
-                    return RedirectToAction("Index", "Home");
-                }
-            }
-            return View(model);
-        }
-        private async Task Authenticate(User user)
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
-                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role?.Name)
-            };
-            ClaimsIdentity id = new ClaimsIdentity(
-                claims,
-                "ApplicationCookie",
-                ClaimsIdentity.DefaultNameClaimType,
-                ClaimsIdentity.DefaultRoleClaimType);
-            await HttpContext.SignInAsync(
-                CookieAuthenticationDefaults.AuthenticationScheme,
-                new ClaimsPrincipal(id),
-                new AuthenticationProperties
-                {
-                    IsPersistent = true,
-                    ExpiresUtc = DateTime.UtcNow.AddMinutes(1)
-                }
-                );
-        }
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Login", "Account");
-        }
+        //private StoreContext db;
+        //public AccountController(StoreContext _db)
+        //{
+        //    db = _db;
+        //}
+        //[HttpGet]
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> Login(LoginViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        User user = await db.Users.Include(u => u.Role).FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+        //        if (user != null)
+        //        {
+        //            await Authenticate(user);
+        //            return RedirectToAction("Index", "Home");
+        //        }
+        //    }
+        //    return View(model);
+        //}
+        //[HttpGet]
+        //public IActionResult Register()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> Register(RegisterViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+        //        if (user == null)
+        //        {
+        //            var role = await db.Roles.FirstOrDefaultAsync(r => r.Name == "user");
+        //            var newuser = new User
+        //            {
+        //                Email = model.Email,
+        //                Password = model.Password,
+        //                Role = role,
+        //                RoleId = role.Id
+        //            };
+        //            await db.Users.AddAsync(newuser);
+        //            await db.SaveChangesAsync();
+        //            await Authenticate(newuser);
+        //            return RedirectToAction("Index", "Home");
+        //        }
+        //    }
+        //    return View(model);
+        //}
+        //private async Task Authenticate(User user)
+        //{
+        //    var claims = new List<Claim>
+        //    {
+        //        new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+        //        new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role?.Name)
+        //    };
+        //    ClaimsIdentity id = new ClaimsIdentity(
+        //        claims,
+        //        "ApplicationCookie",
+        //        ClaimsIdentity.DefaultNameClaimType,
+        //        ClaimsIdentity.DefaultRoleClaimType);
+        //    await HttpContext.SignInAsync(
+        //        CookieAuthenticationDefaults.AuthenticationScheme,
+        //        new ClaimsPrincipal(id),
+        //        new AuthenticationProperties
+        //        {
+        //            IsPersistent = true,
+        //            ExpiresUtc = DateTime.UtcNow.AddMinutes(1)
+        //        }
+        //        );
+        //}
+        //public async Task<IActionResult> Logout()
+        //{
+        //    await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        //    return RedirectToAction("Login", "Account");
+        //}
     }
 }
---
+++ Migrations/20230508044022_AddedUserAndRoleEntityNew.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace home_56.Migrations
+{
+    public partial class AddedUserAndRoleEntityNew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    RoleId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "admin" },
+                    { 2, "user" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Password", "RoleId" },
+                values: new object[] { 1, "admin@admin.admin", "1qwe@QWE", 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
