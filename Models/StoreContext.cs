564cc9f73c3f2f7460b1fbcffc0b50bc4685fa0f
Sultan Moldobekov <gazy_95@mail.ru>
2023-05-08 10:28:01 +0600

    Создана новая миграция. В StoreContext добавлены User и Role, просписан метод OnModelCreating. Создана новая миграция и обновлен БД.

--- Models/StoreContext.cs
+++ Models/StoreContext.cs
@@ -9,6 +9,15 @@ namespace home_56.Models
         public DbSet<Category>? Categories { get; set; }
         public DbSet<Brand>? Brands { get; set; }
         public DbSet<Order>? Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
         public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
+        
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.Entity<Role>().HasData(new Role { Id = 1, Name = "admin" });
+            model.Entity<Role>().HasData(new Role { Id = 2, Name = "user" });
+            model.Entity<User>().HasData(new User { Id = 1, Email = "admin@admin.admin", Password = "1qwe@QWE", RoleId = 1 });
+        }
     }
 }
